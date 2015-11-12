9a31c48c0e47b61374d4aceba1b1a09a445c666f
Ian <iantskon@gmail.com>
2015-11-12 11:34:07 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Angular To-do App
+
+## Instructions
+
+Fill in the missing areas of the code to make it work properly
+
+## Your app should be able to..
+
+- Add a to-do item to a list
+- Display the item instantaneously
+- Clear all items from the list
+- Validate the form using Angular
+
+## BONUS
+
+- Make an 'Add item' button that toggles your form's visibility
+- Hide the form after each item submission
---
+++ app.js
@@ -0,0 +1,11 @@
+app = angular.module('todoApp'); // What's missing? Hint, it's more than one thing.
+
+app.controller("TodosController", function() { // What's missing?
+	$scope.todos = ; // Store todo items here. What is the data type?
+	function addTodo() { // Rewrite to make this function abailable in your view
+		// Add code here
+	};
+	function deleteAll() { // Rewrite to make this function abailable in your view
+		// Add code here
+	};
+});
